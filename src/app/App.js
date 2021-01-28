d5ff0f55340ba752143032f716473f45965279eb
Rinat <ceo@netmoon.ru>
2021-01-28 20:58:45 +0300

    End Lesson 34

---
+++ src/app/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from '../header/Header';
+import PayList from '../pay-list/PayList';
+
+var payItems = [
+  {day: '07', mounth: 'ЯНВ', year: '20', positive: 3000, negative: 543},
+  {day: '23', mounth: 'ФЕВ', year: '20', positive: 5000, negative: 6015},
+  {day: '31', mounth: 'МАР', year: '20', positive: 4000, negative: 5343},
+  {day: '17', mounth: 'АПР', year: '20', positive: 7000, negative: 2043},
+  {day: '12', mounth: 'АВГ', year: '20', positive: 8700, negative: 1000}
+];
+
+function App() {
+  return (
+    <>
+      <Header />
+      <PayList payItems={payItems} />
+    </>
+  );
+}
+
+export default App;
