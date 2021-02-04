4e3e716c02db1c5651f888631430b22cd4115e58
Rinat <ceo@netmoon.ru>
2021-02-04 18:14:11 +0300

    precommit

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from '../header/Header';
 import PayList from '../pay-list/PayList';
 
-var payItems = [
+const payItems = [
   {day: '07', mounth: 'ЯНВ', year: '20', positive: 3000, negative: 543},
   {day: '23', mounth: 'ФЕВ', year: '20', positive: 5000, negative: 6015},
   {day: '31', mounth: 'МАР', year: '20', positive: 4000, negative: 5343},
